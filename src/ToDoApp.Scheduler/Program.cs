b2fa291105a346ef116175a366ece5797a58e211
Anton Grebenkin <grebenkin@itexp.pro>
2023-10-30 14:18:16 +0700

    add files

---
+++ src/ToDoApp.Scheduler/Program.cs
@@ -0,0 +1,10 @@
+using ToDoApp.Scheduler;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
